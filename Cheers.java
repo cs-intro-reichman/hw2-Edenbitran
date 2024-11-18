9578bf814a147edbd7ba941f5ca0ba5bb18092d4
Edenbitran <eden.bitran@post.runi.ac.il>
2024-11-18 19:53:33 +0200

    cheers prog

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,19 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                int times = Integer.parseInt(args[1]);
+                for (int i = 0; i < times; i++) {
+                        if (word.charAt(i) == 'A' || word.charAt(i) == 'E' || word.charAt(i) == 'F'
+                                        || word.charAt(i) == 'H' ||
+                                        word.charAt(i) == 'I' || word.charAt(i) == 'l' || word.charAt(i) == 'M' ||
+                                        word.charAt(i) == 'N' || word.charAt(i) == 'O' || word.charAt(i) == 'R'
+                                        || word.charAt(i) == 'X' || word.charAt(i) == 'S') {
+                                System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                        } else {
+                                System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
+
+                        }
+                }
         }
 }
