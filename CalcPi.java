35cced5b48c8cbfd35e58ed1f8f8d232ea505f5a
Edenbitran <eden.bitran@post.runi.ac.il>
2024-11-21 14:06:08 +0200

    calculate PI

--- CalcPi.java
+++ CalcPi.java
@@ -3,17 +3,22 @@ public class CalcPi {
 	public static void main(String[] args) {
 
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated:     " + sqrt(Double.parseDouble(args[0])));
-	}
+		double number = Double.parseDouble(args[0]);
+		double nn = 1;// nn=new number
+		double count = 1;
 
-	public static double sqrt(double number) {
-		double p = Math.PI;
-		double epsilon = 1 / number;
-		double increment = 0.0001;
-		double sn = 1;// sn- sqrt number
-		while ((Math.abs(sn * sn - p) >= epsilon) && (sn <= p)) {
-			sn += increment;
+		for (int i = 1; i < number; i++) {
+			if (i % 2 == 0) {
+				count += 2;
+				nn += (double) (1 / count);
+			}
+			if (i % 2 != 0) {
+				count += 2;
+				nn -= (double) (1 / count);
+			}
 		}
-		return sn * sn;
+		nn *= 4;
+
+		System.out.println("pi, approximated:     " + nn);
 	}
 }
